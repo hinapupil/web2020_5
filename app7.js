6ee2db4d9d2cf367021e74b239ab527d65817566
hinapupil <hinapupil@gmail.com>
2020-08-05 12:55:40 +0900

    新たに都道府県ごとのID, 名前, 人口性比のデータを取得して動作する動的なページを作成

---
+++ app7.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('test.db');
+
+app.set('view engine', 'ejs');
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  const message = "Hello world";
+  res.render('show', {mes:message});
+});
+
+app.get("/db", (req, res) => {
+    db.serialize( () => {
+        db.all("select id, 都道府県, 人口 from example;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('select', {data:row});
+        })
+    })
+})
+app.get("/top", (req, res) => {
+  //console.log(req.query.pop);    // ①
+  let desc = "";
+  if( req.query.desc ) desc = " desc";
+  let sql = "select id, 都道府県, 人口 from example order by 人口" + desc + " limit " + req.query.pop + ";";
+  //console.log(sql);    // ②
+  db.serialize( () => {
+      db.all(sql, (error, data) => {
+          if( error ) {
+              res.render('show', {mes:"エラーです"});
+          }
+          //console.log(data);    // ③
+          res.render('select', {data:data});
+      })
+  })
+})
+app.get("/gender", (req, res) => {
+    //console.log(req.query.pop);    // ①
+    let desc = "";
+    if( req.query.desc ) desc = " desc";
+    let sql = "select id, 都道府県, (cast(男性 as real) / cast(女性 as real)) * 100 as result from example order by result" + desc + " limit " + req.query.pop + ";";
+    //console.log(sql);    // ②
+    db.serialize( () => {
+        db.all(sql, (error, data) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            //console.log(data);    // ③
+            res.render('select2', {data:data});
+        })
+    })
+})
+app.use(function(req, res, next) {
+  res.status(404).send('ページが見つかりません');
+});
+
+app.listen(8080, () => console.log("Example app listening on port 8080!"));
